0379ac1ac0606e6460c410778d64f7f06e44a0cb
guangyin53222 <532225662@qq.com>
2026-07-22 12:31:45 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,33 +1,39 @@
-# 请在下方输入自定义命令(一般用来安装第三方插件)(可以留空)
-
-# 编辑默认的lan口ip地址
+# ===================== 基础系统修改 =====================
 sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
-
-# 编辑默认的主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# 编辑默认的luci显示的固件名称
-#sed -i 's/OpenWrt/ARWRT/g' package/base-files/files/bin/config_generate
 sed -i 's/ImmortalWrt/ARWRT/g' package/base-files/files/bin/config_generate
 
-# 添加额外的软件包，echo 方式和git clone 方式二选一即可
-#echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-#echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-#echo 'src-git UA3F https://github.com/SunBK201/UA3F.git' >>feeds.conf.default
-#git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
-#git clone https://github.com/kenzok8/small.git package/small
-#git clone https://github.com/SunBK201/UA3F.git package/UA3F
-#git clone https://github.com/stevenjoezhang/luci-app-adguardhome.git package/ADGH
-git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
-# istore插件
-git clone https://github.com/linkease/istore.git package/luci-app-store
-# 克隆gecoosac插件
+# ===================== 第三方插件统一拉取（先删旧残留） =====================
+rm -rf package/OpenAppFilter
+git clone --depth 1 -b v6.1.8 https://github.com/destan19/OpenAppFilter package/OpenAppFilter
+
+rm -rf package/luci-app-store
+git clone --depth=1 https://github.com/linkease/istore.git package/luci-app-store
+
+rm -rf package/luci-app-gecoosac
 git clone --depth=1 https://github.com/laipeng668/luci-app-gecoosac package/luci-app-gecoosac
-#下载 wan-mac 修改插件
+
+rm -rf tmp/openwrt-app-actions package/luci-app-wan-mac
 git clone --depth=1 https://github.com/linkease/openwrt-app-actions tmp/openwrt-app-actions
 mv tmp/openwrt-app-actions/applications/luci-app-wan-mac package/luci-app-wan-mac
 rm -rf tmp/openwrt-app-actions
-# Bandix-plus 流量监控：后端核心程序
-#git clone --depth=1 https://github.com/timsaya/openwrt-bandix-plus package/openwrt-bandix-plus
-# Bandix-plus 流量监控：LuCI前端界面
-#git clone --depth=1 https://github.com/timsaya/luci-app-bandix-plus package/luci-app-bandix-plus
+
+# ===================== Tailscale v1.98.9 自定义 =====================
+rm -rf package/feeds/tailscale package/custom_tailscale
+git clone --depth 1 https://github.com/tailscale/tailscale-openwrt.git package/custom_tailscale
+
+sed -i 's/^PKG_VERSION:=.*/PKG_VERSION:=1.98.9/' package/custom_tailscale/Makefile
+sed -i 's/^PKG_SOURCE:=.*/PKG_SOURCE:=v1.98.9.tar.gz/' package/custom_tailscale/Makefile
+sed -i 's|^PKG_SOURCE_URL:=.*|PKG_SOURCE_URL:=https://github.com/tailscale/tailscale/archive/refs/tags|' package/custom_tailscale/Makefile
+
+sed -i '/EXTRA_GOFLAGS/d' package/custom_tailscale/Makefile
+echo 'EXTRA_GOFLAGS += -tags=ts_no_metrics' >> package/custom_tailscale/Makefile
+
+sed -i '/tailscale up /c\tailscale up \\\n--advertise-routes=192.168.100.0/24 \\\n--accept-routes \\\n--accept-dns=false \\\n--snat-subnet-routes=false \\\n--netfilter-mode=off' package/custom_tailscale/files/tailscale.init
+
+echo 'src-link tailscale $(TOPDIR)/package/custom_tailscale' >> feeds.conf.default
+rm -rf dl/tailscale* build_dir/go bin/packages/*/tailscale
+
+# 更新并安装全部插件
+./scripts/feeds update -a
+./scripts/feeds install -a
