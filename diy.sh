7d6e1922b7a4c189857db957b8398972ab5225f7
577090447 <577090447@qq.com>
2026-07-31 22:32:45 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,14 +1,14 @@
 # 请在下方输入自定义命令(一般用来安装第三方插件)(可以留空)
 
 # 编辑默认的lan口ip地址
-sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 
 # 编辑默认的主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # 编辑默认的luci显示的固件名称
 #sed -i 's/OpenWrt/ZWRT/g' package/base-files/files/bin/config_generate
-#sed -i 's/ImmortalWrt/ZWRT/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/arWRT/g' package/base-files/files/bin/config_generate
 
 # 添加额外的软件包，echo 方式和git clone 方式二选一即可
 #echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
@@ -17,4 +17,23 @@ echo 'src-git UA3F https://github.com/SunBK201/UA3F.git' >>feeds.conf.default
 #git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
 #git clone https://github.com/kenzok8/small.git package/small
 #git clone https://github.com/SunBK201/UA3F.git package/UA3F
-git clone https://github.com/stevenjoezhang/luci-app-adguardhome.git package/ADGH
+#git clone https://github.com/stevenjoezhang/luci-app-adguardhome.git package/ADGH
+# ===================== 第三方插件统一拉取（先删旧残留） =====================
+rm -rf package/OpenAppFilter
+git clone --depth 1 -b v6.1.8 https://github.com/destan19/OpenAppFilter package/OpenAppFilter
+
+rm -rf package/luci-app-store
+git clone --depth=1 https://github.com/linkease/istore.git package/luci-app-store
+
+rm -rf package/luci-app-gecoosac
+git clone --depth=1 https://github.com/laipeng668/luci-app-gecoosac package/luci-app-gecoosac
+
+rm -rf tmp/openwrt-app-actions package/luci-app-wan-mac
+git clone --depth=1 https://github.com/linkease/openwrt-app-actions tmp/openwrt-app-actions
+mv tmp/openwrt-app-actions/applications/luci-app-wan-mac package/luci-app-wan-mac
+rm -rf tmp/openwrt-app-actions
+# 添加 luci-app-tcpdump 抓包插件
+git clone https://github.com/KFERMercer/luci-app-tcpdump.git ./package/luci-app-tcpdump
+# 更新并安装插件源
+./scripts/feeds update -a
+./scripts/feeds install -a
