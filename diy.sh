34ad9ffc115f2896f33aeb0c3a9fadde9fc327e4
guangyin53222 <532225662@qq.com>
2026-08-05 11:35:54 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,37 +1,20 @@
-#!/bin/bash
+# 请在下方输入自定义命令(一般用来安装第三方插件)(可以留空)
 
-# ============================================================
-# diy.sh - iStoreOS / ImmortalWRT 自定义脚本
-# 执行时机：在 feeds update 之前
-# ============================================================
+# 编辑默认的lan口ip地址
+sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 
-# ==================== 基础系统定制 ====================
+# 编辑默认的主题
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# 修改默认 LAN IP 为 192.168.100.1
-sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
+# 编辑默认的luci显示的固件名称
+#sed -i 's/OpenWrt/ZWRT/g' package/base-files/files/bin/config_generate
+#sed -i 's/ImmortalWrt/ZWRT/g' package/base-files/files/bin/config_generate
 
-# 修改固件显示名称 ImmortalWrt → agWRT
-sed -i 's/ImmortalWrt/agWRT/g' package/base-files/files/bin/config_generate
-
-echo "✅ 基础系统定制完成"
-
-# ==================== 第三方插件克隆 ====================
-# ⚠️ 必须在 feeds update 之前 clone 到 package/ 目录下
-
-# 集客 AC 控制器
-echo "📦 克隆 luci-app-gecoosac ..."
-rm -rf package/luci-app-gecoosac
-git clone --depth=1 https://github.com/laipeng668/luci-app-gecoosac package/luci-app-gecoosac
-
-# tcpdump 抓包插件
-echo "📦 克隆 luci-app-tcpdump ..."
-rm -rf package/luci-app-tcpdump
-git clone --depth=1 https://github.com/KFERMercer/luci-app-tcpdump.git package/luci-app-tcpdump
-
-# Harbor File 文件管理器
-echo "📦 克隆 luci-app-harbor-file ..."
-rm -rf package/luci-app-harbor-file
-git clone --depth=1 https://github.com/destan19/luci-app-harbor-file.git package/luci-app-harbor-file
-
-echo "✅ 第三方插件克隆完成"
-echo "✅ diy.sh 执行完成"
+# 添加额外的软件包，echo 方式和git clone 方式二选一即可
+#echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+#echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git UA3F https://github.com/SunBK201/UA3F.git' >>feeds.conf.default
+#git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
+#git clone https://github.com/kenzok8/small.git package/small
+#git clone https://github.com/SunBK201/UA3F.git package/UA3F
+git clone https://github.com/stevenjoezhang/luci-app-adguardhome.git package/ADGH
